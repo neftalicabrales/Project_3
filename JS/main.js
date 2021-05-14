c02821a2728dd43a1e2a921c7acd89c9256f9b68
neftalicabrales <neftalicabrales@gmail.com>
2021-05-14 15:17:40 -0400

    project

---
+++ JS/main.js
@@ -0,0 +1,61 @@
+let options = {
+    root: null,
+    threshold: 0.25,
+  }
+
+  function intersectionHandler(entries, observer) {
+entries.forEach(entry => {
+
+    if (entry.isIntersecting) {
+        entry.target.classList.add("active"); 
+
+        let color = entry.target.dataset.color;
+       document.body.style.backgroundColor = color;
+    } else {
+        entry.target.classList.remove("active");
+       
+    }
+  });
+  }
+
+  let observer = new IntersectionObserver(intersectionHandler, options);
+
+  let images =  document.querySelectorAll("img");
+
+  images.forEach((image) => {
+observer.observe(image);
+ });
+
+ var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
